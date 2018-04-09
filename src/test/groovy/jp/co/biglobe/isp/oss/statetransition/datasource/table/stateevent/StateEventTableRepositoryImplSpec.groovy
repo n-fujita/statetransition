4dc9ae3ed4bfc6b1832eb2d1cf8f647ce71f7967
naosim <>
2018-04-10 05:32:04 +0900

    リファクタ builder化

--- src/test/groovy/jp/co/biglobe/isp/oss/statetransition/datasource/table/stateevent/StateEventTableRepositoryImplSpec.groovy
+++ src/test/groovy/jp/co/biglobe/isp/oss/statetransition/datasource/table/stateevent/StateEventTableRepositoryImplSpec.groovy
@@ -1,16 +1,13 @@
 package jp.co.biglobe.isp.oss.statetransition.datasource.table.stateevent
 
 import jp.co.biglobe.isp.oss.statetransition.datasource.DbSpecCommon
-import jp.co.biglobe.isp.oss.statetransition.datasource.StateEvent
-import jp.co.biglobe.isp.oss.statetransition.datasource.StateEventId
-import jp.co.biglobe.isp.oss.statetransition.domain.StateEventDateTime
 import jp.co.biglobe.isp.oss.statetransition.sample.SampleStateType
-import jp.co.biglobe.isp.oss.statetransition.sample.contract.ContractState
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.TestConfiguration
 
-import java.time.LocalDateTime
+import static jp.co.biglobe.isp.oss.statetransition.datasource.table.stateevent.InsertStateEventContainerBuilder.契約
+import static jp.co.biglobe.isp.oss.statetransition.datasource.table.stateevent.InsertStateEventContainerBuilder.申込
 
 @SpringBootTest
 @TestConfiguration
@@ -18,80 +15,41 @@ class StateEventTableRepositoryImplSpec extends DbSpecCommon {
     @Autowired
     StateEventTableRepository sut
 
-    def "insertAndFind"() {
+    def "挿入したイベントが取れること"() {
+        setup:
+        def 申込Container = 申込("EVENT001").build()
+        def 申込Event = 申込("EVENT001").buildToStateEvent(false)
+
         when:
-        def container = new InsertStateEventContainer(
-                new StateEventId("EVENT001"),
-                "ID001",
-                SampleStateType.contract,
-                ContractState.ordered,
-                new StateEventDateTime(LocalDateTime.of(2017, 12, 30, 0, 0)),
-                LocalDateTime.of(2018, 1, 1, 0, 0)
-        )
-        sut.insertStateEvent(container)
+        sut.insertStateEvent(申込Container)
 
         // 挿入したものが取れること
         def act = sut.findAllEvent(new FindLatestContainer("ID001", SampleStateType.contract))
 
         then:
-        act.size() == 1
-        act == [
-                new StateEvent(
-                    new StateEventId("EVENT001"),
-                    "ID001",
-                    ContractState.ordered,
-                    SampleStateType.contract,
-                    new StateEventDateTime(LocalDateTime.of(2017, 12, 30, 0, 0)),
-                    LocalDateTime.of(2018, 1, 1, 0, 0),
-                    false
-                )
-        ]
+        act == [申込Event]
     }
 
-    def "insert2AndFind"() {
+    def "複数挿入した場合、StateEventId順に取得できること"() {
+        setup:
+        def 申込Container = 申込("EVENT001").build()
+        def 契約Container = 契約("EVENT002").stateEventDateTime(2018, 2, 1).eventDateTime(2018, 3, 1).build()
+
+        def 申込Event = 申込("EVENT001").buildToStateEvent(false)
+        def 契約Event = 契約("EVENT002").stateEventDateTime(2018, 2, 1).eventDateTime(2018, 3, 1).buildToStateEvent(false)
+
         when:
-        def container = new InsertStateEventContainer(
-                new StateEventId("EVENT001"),
-                "ID001",
-                SampleStateType.contract,
-                ContractState.ordered,
-                new StateEventDateTime(LocalDateTime.of(2017, 12, 30, 0, 0)),
-                LocalDateTime.of(2018, 1, 1, 0, 0)
-        )
-        def container2 = new InsertStateEventContainer(
-                new StateEventId("EVENT002"),
-                "ID001",
-                SampleStateType.contract,
-                ContractState.contracted,
-                new StateEventDateTime(LocalDateTime.of(2018, 2, 1, 0, 0)),
-                LocalDateTime.of(2018, 3, 1, 0, 0)
-        )
-        sut.insertStateEvent(container)
-        sut.insertStateEvent(container2)
+        sut.insertStateEvent(契約Container)
+        sut.insertStateEvent(申込Container)
+
 
         // 挿入したものが取れること
         def act = sut.findAllEvent(new FindLatestContainer("ID001", SampleStateType.contract))
 
         then:
         act == [
-                new StateEvent(
-                        new StateEventId("EVENT001"),
-                        "ID001",
-                        ContractState.ordered,
-                        SampleStateType.contract,
-                        new StateEventDateTime(LocalDateTime.of(2017, 12, 30, 0, 0)),
-                        LocalDateTime.of(2018, 1, 1, 0, 0),
-                        false
-                ),
-                new StateEvent(
-                        new StateEventId("EVENT002"),
-                        "ID001",
-                        ContractState.contracted,
-                        SampleStateType.contract,
-                        new StateEventDateTime(LocalDateTime.of(2018, 2, 1, 0, 0)),
-                        LocalDateTime.of(2018, 3, 1, 0, 0),
-                        false
-                )
+                申込Event,
+                契約Event
         ]
     }
 }
